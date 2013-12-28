3dbeac53de375bbc166746ecb368d6bf18e88c76
evgwed <evgwed@gmail.com>
2013-12-28 17:47:00 +0400

    Beta v.1

--- index.php
+++ index.php
@@ -1,30 +1,43 @@
-<h1>Addurl для анализаторов сайта</h1>
-<script src="js/jquery-2.0.2.min.js"></script>
-<script src="js/script.js"></script>
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-
-<?php
-	$lines = file('base/base.bd');
-	$sites= "";
-	foreach ($lines as $line_num => $line) {
-		$sites.="'".trim($line)."', ";
-	}
-?>
-<script>
-	var sites = [<?=$sites?>];
-</script>
-<div id="frame_b">
-	<iframe src ="" name= "one" scrolling="no" sandbox="allow-same-origin"  id="frame_one"></iframe>
-</div>
-<div id="added_site">
-	<h2>
-		Добавленные сайты:
-	</h2>
-	<div class="addedsite">
-	</div>	
-	<div class="addedsite">
-	</div>	
-	<div class="addedsite">
-	</div>	
-</div>
-<a href="javascript:void(0)" id="start">Start</a>
+<html>
+<head>
+    <script src="js/jquery-2.0.2.min.js"></script>
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <?php
+    $lines = file('base/base.bd');
+    $sites= "";
+    foreach ($lines as $line_num => $line) {
+        $sites.="'".trim($line)."', ";
+    }
+    ?>
+    <script>
+        var sites = [<?=$sites?>];
+    </script>
+</head>
+<body>
+    <div class="body_b">
+        <h1 class="logo">Бесплатный сервис прогона сайтов по сайтам-анализаторам</h1>
+        <a href="http://catine.ru/" target="_blank" class="copyright">Разработано Catine.ru</a>
+        <div id="frame_b">
+            <iframe src ="none.html" name= "one" scrolling="no" sandbox="allow-same-origin" seamless id="frame_one"></iframe>
+            <input name="site_url" id="site_url" type="text" value="" placeholder="Введите сайт для прогона" />
+            <p class="descript_url">Пример ввода сайта: google.ru</p>
+            <fieldset class="switch">
+                <label class="off">Выкл<input type="radio" class="on_off" name="on_off" value="off"/></label>
+                <label class="on">Вкл<input type="radio" class="on_off" name="on_off" value="on"/></label>
+            </fieldset>
+        </div>
+        <div id="added_site">
+            <h2>
+                Добавленные сайты:
+            </h2>
+            <div class="addedsite">
+            </div>
+            <div class="addedsite">
+            </div>
+            <div class="addedsite">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
-var index = 1;
+var index = 0;
 var index_append_sites = 0;
+var on_app = false;
+var first_start = true;
+var site_url = "";
 $(document).ready(function(){
-	$("#start").click(function(){
-		$("#frame_one").attr("onload", "setTimeout('next_site()', 1000)");
-		$("#frame_one").attr("src", sites[index]+"google.ru");
-		$("#added_site .addedsite").eq(index_append_sites).append("<p><a href="+sites[index]+"google.ru"+" target='_blank'>"+sites[index]+"google.ru"+"</a></p>");
-		index++;
-	});
+    $('.switch').css('background-image', 'url("img/switch.png")');
+    $('.on_off').css('display','none');
+    $('.on, .off').css('text-indent','-10000px');
+
+    $("input[name=on_off]").change(function() {
+        var button = $(this).val();
+        if(button == 'off'){
+            $('.switch').css('background-position', 'right');
+            on_app= false;
+        }
+        if(button == 'on'){
+            if(first_start)
+            {
+                first_start=false;
+                site_url = $("#site_url").val();
+                $("#frame_one").attr("onload", "setTimeout('next_site()', 1000)");
+            }
+            $("#frame_one").attr("src", sites[index]+site_url);
+            $("#added_site .addedsite").eq(index_append_sites).append("<p><a href='"+sites[index]+site_url+"' target='_blank'>"+sites[index]+site_url+"</a></p>");
+            index++;
+            $('.switch').css('background-position', 'left');
+            on_app= true;
+        }
+
+    });
+    $("#site_url").keypress(function(e){
+        if(e.keyCode==13){
+            alert("Для запуска необходимо переключить выключетель ниже в состоянии ON.");
+        }
+    });
 });
 	function next_site(){
-		if(index<sites.length)
+		if(index<sites.length&&on_app)
 		{
-			$("#frame_b iframe").eq(0).attr("src", sites[index]+"google.ru");
+			$("#frame_b iframe").eq(0).attr("src", sites[index]+site_url);
 			if(index>=sites.length/3&&index<sites.length*2/3)
 			{
 				index_append_sites = 1;
@@ -20,11 +47,12 @@ $(document).ready(function(){
 			{
 				index_append_sites = 2;
 			}
-			$("#added_site .addedsite").eq(index_append_sites).append("<p><a href="+sites[index]+"google.ru"+" target='_blank'>"+sites[index]+"google.ru"+"</a></p>");
+			$("#added_site .addedsite").eq(index_append_sites).append("<p><a href='"+sites[index]+site_url+"' target='_blank'>"+sites[index]+site_url+"</a></p>");
 			index++;
 		}
 		else
 		{
-			alert("Complete");
-		}
+            $('.switch').css('background-position', 'right');
+            on_app= false;
+        }
 	}
